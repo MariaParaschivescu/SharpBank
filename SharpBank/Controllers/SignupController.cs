4561cece1b2a038e49780851fd0646ec96ec2e4c
Remus Roman <remusroman99@yahoo.com>
2021-03-14 01:34:38 +0200

    SB-8: Create login view: - Create login view - Create signup view SB-10: Create login controller: - Create login controller SB-12: Validate user credentials: - Add validation for empty input values on submit and invalid credentials

---
+++ SharpBank/Controllers/SignupController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SharpBank.Models.Login;
+
+namespace SharpBank.Controllers
+{
+    public class SignupController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SendSignupData(SignupFormModel signupForm)
+        {
+            //TODO check for existing users in bank database and if the request is valid, create account, send validation
+            // and show signup success
+            return View();
+        }
+    }
+}
