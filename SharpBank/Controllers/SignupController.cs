196462c4ce21c019c1fba21791d1d24761b26b93
Remus Roman <remusroman99@yahoo.com>
2021-03-14 13:46:20 +0200

    WIP SB-12: Validate signup user credentials: - Add password strength checker

--- SharpBank/Controllers/SignupController.cs
+++ SharpBank/Controllers/SignupController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SharpBank.Models;
 using SharpBank.Models.Login;
 
 namespace SharpBank.Controllers
@@ -14,10 +16,17 @@ namespace SharpBank.Controllers
             return View();
         }
 
-        public IActionResult SendSignupData(SignupFormModel signupForm)
+        [HttpPost]
+        public IActionResult Signup(SignupFormModel signupForm)
         {
             //TODO check for existing users in bank database and if the request is valid, create account, send validation
             // and show signup success
+            ErrorViewModel error = new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier,
+                ErrorMessage = "Signup failed due to ..."
+            };
+            ViewBag.Message = error;
             return View();
         }
     }
